172412135f9da97342f43783ac536aaa94600a4b
Vladislav Ganev <vladislavganev@gmail.com>
2018-01-15 22:11:26 +0200

    Streams - Exercises

---
+++ CSharp Advanced/Streams-Exercise/01. Odd Lines/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a program that reads a text file and prints on the console its odd lines. Line numbers starts from 0. Use StreamReader.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"..\..\somefile.txt";
+            using (StreamReader reader = new StreamReader(file))
+            {
+                int lineNumber = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    lineNumber++;
+                    if (lineNumber %2 ==0)
+                    {
+                        Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Streams-Exercise/02. Line Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"..\..\sourcefile.txt";
+            string destinationFile = @"..\..\destfile.txt";
+            using (StreamReader reader = new StreamReader(sourceFile))
+            {
+                using (var writer = new StreamWriter(destinationFile))
+                {
+                    long lineNumber = 1;
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {                       
+                        writer.WriteLine($"Line {lineNumber}: {line}");
+                        lineNumber++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
