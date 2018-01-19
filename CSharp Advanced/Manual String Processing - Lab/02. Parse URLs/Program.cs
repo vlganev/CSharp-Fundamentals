aa7ef73a5d9ba60a00580eda340555d8d43125f0
Vladislav Ganev <vladislavganev@gmail.com>
2018-01-19 21:58:42 +0200

    Manual string processin - lab commit

---
+++ CSharp Advanced/Manual String Processing - Lab/02. Parse URLs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_URLs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = Console.ReadLine();
+
+            string separator = "://";
+
+            string[] urlTokens = url.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (urlTokens.Length != 2 || urlTokens[1].IndexOf('/') == -1)
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+            else
+            {
+                string protocol = urlTokens[0];
+                int indexResourse = urlTokens[1].IndexOf('/');
+                string server = urlTokens[1].Substring(0, indexResourse);
+                string resources = urlTokens[1].Substring(indexResourse+1);
+                Console.WriteLine($"Protocol = {protocol}");
+                Console.WriteLine($"Server = {server}");
+                Console.WriteLine($"Resources = {resources}");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Manual String Processing - Lab/03. Parse Tags/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_Tags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string openTag = "<upcase>";
+            string closedTag = "</upcase>";
+
+            int startIndex = input.IndexOf(openTag);
+            while (startIndex != -1)
+            {
+                int endIndex = input.IndexOf(closedTag);
+                if (endIndex == -1)
+                {
+                    break;
+                }
+                string toBeReplaced = input.Substring(startIndex, endIndex + closedTag.Length - startIndex);
+                var replaced = toBeReplaced.Replace(openTag, string.Empty).Replace(closedTag, string.Empty).ToUpper();
+
+                input = input.Replace(toBeReplaced, replaced);
+
+                startIndex = input.IndexOf(openTag);
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
