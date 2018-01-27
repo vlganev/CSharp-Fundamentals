6be3a97b9d6b63d12261fae5e6de9b0862ca6f74
Vladislav Ganev <vladislavganev@gmail.com>
2018-01-27 11:32:52 +0200

    CSharp OOP - defining classes Lab

---
+++ CSharp OOP Basics/Define Bank Account Class/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private List<BankAccount> accounts;
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+    public List<BankAccount> Accounts
+    {
+        get { return this.accounts; }
+        set { this.accounts = value; }
+    }
+
+    public Person(string name, int age)
+        : this(name, age, new List<BankAccount>())
+    { }
+
+    public Person(string name, int age, List<BankAccount> accounts)
+    {
+        this.name = name;
+        this.age = age;
+        this.accounts = accounts;
+    }
+
+    public double GetBalance()
+    {
+        return this.accounts.Select(a => a.Balance).Sum();
+    }
+}
