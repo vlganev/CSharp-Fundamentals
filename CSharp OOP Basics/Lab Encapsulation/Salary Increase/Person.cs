6f17489243554ef4522723920391180cad7e80c7
Vladislav Ganev <vladislavganev@gmail.com>
2018-01-28 11:09:00 +0200

    CSharp OOP encapsulation lab commit

---
+++ CSharp OOP Basics/Lab Encapsulation/Salary Increase/Person.cs
@@ -0,0 +1,43 @@
+﻿
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+    }
+
+    public int Age
+    {
+        get { return this.age;  }
+    }
+
+    public void IncreaseSalary(double bonus)
+    {
+        if(this.Age < 30)
+        {
+            this.salary += this.salary * bonus / 200;
+        }
+        else
+        {
+            this.salary += this.salary * bonus / 100;
+        }
+    } 
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary} leva";
+    }
+}
