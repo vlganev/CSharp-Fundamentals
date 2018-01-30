0ee2f10e3c156e8e670763ba3c0e1319a256e075
Vladislav Ganev <vladislavganev@gmail.com>
2018-01-30 18:18:26 +0200

    C# OOP Advanced Interfaces and Abstraction Lab commit.

---
+++ CSharp OOP Advanced/Interfaces and Abstraction Lab/2. Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public string Model { get; }
+
+    public string Color { get; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} {this.GetType().Name} {this.Model}";
+    }
+}
---
+++ CSharp OOP Advanced/Interfaces and Abstraction Lab/2. Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        this.Battery = battery;
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public int Battery { get; }
+    public string Model { get; }
+
+    public string Color { get; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries";
+    }
+}
